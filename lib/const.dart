1f4cf9a69f83278dbc972af7e8d840ea9ef56a7e
kerimthegreat <keamansaryyev@example.com>
2021-08-04 12:56:11 +0500

    Changed the backend of the app

--- lib/const.dart
+++ lib/const.dart
@@ -1,8 +1,8 @@
 // NETWORK
-const BASE_URL = 'https://takykcesme.com.tm:8443/backend/mobile/api/';
-const REGISTER_NUMBER = 'register-app';
-const CHECK_NUMBER = 'check-app';
+const BASE_URL = "http://95.85.122.117/api/";
+const REGISTER_NUMBER = 'sms';
 const POINT_ID = 1;
-const POINT_NUMBER = '+99361100343';
+const GATEAWAY_PHONE = '99361100343';
+const TOKEN = "E6HUEhG8GTC2PO7HIIWpr1MrvaUBlKr7";
 
 const kDefaultResultsStorageFolder = 'results';
--- lib/network.dart
+++ lib/network.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:dio/adapter.dart';
 import 'package:dio/dio.dart';
 
 import 'const.dart';
@@ -11,22 +14,32 @@ class Network {
     _dio.options.sendTimeout = 5000;
     _dio.options.connectTimeout = 5000;
     _dio.options.receiveTimeout = 5000;
+    (_dio.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate =
+        (HttpClient client) {
+      client.badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+      return client;
+    };
   }
 
   Future<bool> registerNumber(String? phoneNumber, String? code) async {
-    final timestamp = DateTime.now().toUtc().millisecondsSinceEpoch ~/ 1000;
-    final data = FormData.fromMap({
-      'point_id': POINT_ID,
-      'point_phone': POINT_NUMBER,
-      'phone': phoneNumber,
-      'message': code,
-      'date_incoming': timestamp,
-      'test': 1,
-    });
-    final response = await _dio.post(REGISTER_NUMBER, data: data);
-    print(response.statusCode);
-    print(response.data);
-    if (response.statusCode == 200) return true;
+    final data = FormData.fromMap(
+       {
+         'gateway_phone': GATEAWAY_PHONE.toString(),
+         'user_phone': phoneNumber.toString().replaceAll('+', ""),
+         'content': code.toString(),
+         'gateway_token':TOKEN
+       }
+    );
+    Response? response;
+    try {
+      response = await _dio.post(REGISTER_NUMBER, data: data);
+    } catch (e) {
+      print('error $e');
+    }
+    print(response?.statusCode);
+    print(response?.data);
+    if (response?.statusCode == 201) return true;
     return false;
   }
 }
